2b2ceebbe3fefb7ba014077fd9b3a4ceee00fd03
surya prakash chamoli <suryaprakashchamoli@gmail.com>
2026-07-29 16:47:11 +0530

    seprate custmoer table from resoucers

--- app/Filament/Resources/Customers/Tables/CustomersTable.php
+++ app/Filament/Resources/Customers/Tables/CustomersTable.php
@@ -11,16 +11,22 @@ use Filament\Tables\Columns\TextColumn;
 use App\Models\Employee;
 use App\Filament\Resources\FollowUps\FollowUpResource;
 use Filament\Actions\Action;
-
+use App\Filament\Exports\CustomerExporter;
+use Filament\Actions\ExportAction;
+use Filament\Actions\ExportBulkAction;
+use Filament\Actions\ImportAction;
+use App\Filament\Imports\CustomerImporter;
+use Filament\Tables\Filters\SelectFilter;
 
 class CustomersTable
 {
     public static function configure(Table $table): Table
     {
         return $table
+            ->defaultSort('id', 'desc')
             ->columns([
                 //
-                 TextColumn::make('application_no')
+                TextColumn::make('application_no')
                     ->label('Application No')
                     ->searchable()
                     ->sortable(),
@@ -32,8 +38,7 @@ class CustomersTable
 
                 TextColumn::make('customer_name')
                     ->label('Customer Name')
-                    // ->searchable()
-                    ->searchable(isIndividual: true , isGlobal: false)
+                    ->searchable()
                     ->sortable(),
 
                 TextColumn::make('sanctioned_loan_amount')
@@ -100,8 +105,24 @@ class CustomersTable
 
             ])
             ->filters([
-                //
+                SelectFilter::make('journey_status')
+                    ->label('Journey Status')
+                    ->options([
+                        'sfl' => 'SFL',
+                        'underwriting' => 'Underwriting',
+                        'approved' => 'Approved',
+                        'sanctioned' => 'Sanctioned',
+                        'disbursal_documents' => 'Disbursal Documents',
+                        'completed' => 'Completed',
+                        'carry_forward' => 'Carry Forward',
+                        'dropped' => 'Dropped',
+                        'not_approved' => 'Not Approved',
+                    ])
+                    ->searchable()
+                    ->preload(),
             ])
+            ->defaultPaginationPageOption(5)
+            ->paginated([5, 10, 25, 50, 100, 'all'])
             ->recordActions([
                 ViewAction::make(),
                 // EditAction::make(),
@@ -109,21 +130,42 @@ class CustomersTable
                     ->visible(
                         fn($record) =>
                         // ! $record->documents_submitted &&
-                            auth()->user()->employee?->designation !== Employee::DESIGNATION_CALLER
+                        auth()->user()->employee?->designation !== Employee::DESIGNATION_CALLER
                     ),
 
                 Action::make('followup')
-                        ->label('Follow Up')
-                        ->icon('heroicon-o-phone')
-                        ->color('warning')
-                        ->url(fn($record) => FollowUpResource::getUrl('create', [
-                            'customer' => $record->id,
-                        ])),
+                    ->label('Follow Up')
+                    ->icon('heroicon-o-phone')
+                    ->color('warning')
+                    ->url(fn($record) => FollowUpResource::getUrl('create', [
+                        'customer' => $record->id,
+                    ])),
+            ])
+            ->headerActions([
+
+                ExportAction::make()
+                    ->exporter(CustomerExporter::class)
+                    ->label('Export Customers')
+                    ->icon('heroicon-o-arrow-down-tray')
+                    ->visible(fn() => auth()->user()->hasRole('Admin')),
+
+                ImportAction::make()
+                    ->label('Import Customers')
+                    ->icon('heroicon-o-arrow-up-tray')
+                    ->color('primary')
+                    ->importer(CustomerImporter::class)
             ])
             ->toolbarActions([
-                BulkActionGroup::make([
+                // BulkActionGroup::make([
+                // DeleteBulkAction::make(),
+                // ]),
+
                 DeleteBulkAction::make(),
-                ]),
+                ExportBulkAction::make()
+                    ->exporter(CustomerExporter::class)
+                    ->label('Export Selected'),
+
+
             ]);
     }
 }
