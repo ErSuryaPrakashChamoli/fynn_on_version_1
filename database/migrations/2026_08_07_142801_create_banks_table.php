ac411f311f9f27c313ea905570a3314472b443aa
surya prakash chamoli <suryaprakashchamoli@gmail.com>
2026-08-07 14:33:33 +0530

    update bank migration

---
+++ database/migrations/2026_08_07_142801_create_banks_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('banks', function (Blueprint $table) {
+            $table->id();
+            $table->string('bank_name');
+            $table->string('loan_type');
+
+            // Source (DSA / Connector / Branch / API Partner)
+            // $table->string('source')->nullable();
+            $table->string('payment_from')->nullable();
+
+            // Payout Percentage
+            $table->decimal('payout', 5, 2)->default(0.00);
+
+            $table->boolean('is_active')->default(true);
+
+            $table->timestamps();
+
+            // Prevent duplicate product configuration
+            $table->unique([
+                'bank_name',
+                'loan_type',
+                'source',
+            ], 'bank_product_unique');
+            // $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('banks');
+    }
+};
