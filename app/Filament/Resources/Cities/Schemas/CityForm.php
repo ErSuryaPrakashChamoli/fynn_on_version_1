55c8ad3c4eadcf6b8a5e85aad8a3a69d9df09523
surya prakash chamoli <suryaprakashchamoli@gmail.com>
2026-08-07 14:08:11 +0530

    update city validation

--- app/Filament/Resources/Cities/Schemas/CityForm.php
+++ app/Filament/Resources/Cities/Schemas/CityForm.php
@@ -14,24 +14,6 @@ class CityForm
     public static function configure(Schema $schema): Schema
     {
 
-
-        // $city = City::where('city', $office['District'])
-        //     ->where('state', $office['State'])
-        //     ->first();
-
-
-
-        // if (!$city) {
-
-        //     $city = City::create([
-        //         'country' => 'India',
-        //         'state' => $office['State'],
-        //         'city' => $office['District'],
-        //     ]);
-        // }
-
-
-
         return $schema
             ->components([
                 //
@@ -41,18 +23,10 @@ class CityForm
                     ->default('India')
                     ->maxLength(255),
 
-                // TextInput::make('state_code')
-                //     ->label('State Code')
-                //     ->maxLength(10),
-
-                // TextInput::make('city_code')
-                //     ->label('City Code')
-                //     ->maxLength(10),
-
-
                 TextInput::make('pincode')
                     ->label('PIN Code')
                     ->numeric()
+                     ->required()
                     ->maxLength(6)
                     ->live(debounce: 700)
                     ->suffixIcon('heroicon-o-arrow-path')
@@ -63,70 +37,6 @@ class CityForm
                         'wire:loading.class' => 'animate-spin',
                         'wire:target' => 'data.pincode',
                     ])
-
-
-
-                    // ->afterStateUpdated(function ($state, callable $set) {
-
-                    //     if (strlen($state) != 6) {
-                    //         return;
-                    //     }
-
-                    //     $url = "https://api.postalpincode.in/pincode/{$state}";
-
-                    //     $ch = curl_init();
-
-                    //     curl_setopt_array($ch, [
-                    //         CURLOPT_URL => $url,
-                    //         CURLOPT_RETURNTRANSFER => true,
-                    //         CURLOPT_FOLLOWLOCATION => true,
-                    //         CURLOPT_TIMEOUT => 15,
-                    //         CURLOPT_SSL_VERIFYPEER => false, // remove in production if possible
-                    //         CURLOPT_USERAGENT => 'FYNN-ON/1.0',
-                    //     ]);
-
-                    //     $result = curl_exec($ch);
-
-
-
-                    //     if (curl_errno($ch)) {
-
-                    //         \Filament\Notifications\Notification::make()
-                    //             ->title('Curl Error')
-                    //             ->body(curl_error($ch))
-                    //             ->danger()
-                    //             ->send();
-
-                    //         curl_close($ch);
-                    //         return;
-                    //     }
-
-                    //     curl_close($ch);
-
-                    //     $data = json_decode($result, true);
-
-                    //     if (
-                    //         isset($data[0]['Status']) &&
-                    //         $data[0]['Status'] === 'Success' &&
-                    //         ! empty($data[0]['PostOffice'])
-                    //     ) {
-
-                    //         $office = $data[0]['PostOffice'][0];
-
-                    //         $set('city', $office['District']);
-                    //         $set('state', $office['State']);
-                    //     } else {
-
-                    //         $set('city', null);
-                    //         $set('state', null);
-
-                    //         \Filament\Notifications\Notification::make()
-                    //             ->title('Invalid PIN Code')
-                    //             ->danger()
-                    //             ->send();
-                    //     }
-                    // })
-
                     ->afterStateUpdated(function ($state, callable $set) {
 
                         if (strlen($state) != 6) {
@@ -145,8 +55,8 @@ class CityForm
                         }
 
                         // 2. Not found, call API
+                        // $url = "https://api.postalpincode.in/pincode/{$state}";
                         $url = "https://api.postalpincode.in/pincode/{$state}";
-
                         $ch = curl_init();
 
                         curl_setopt_array($ch, [
@@ -222,10 +132,12 @@ class CityForm
 
                 TextInput::make('city')
                     ->readOnly()
+                    ->required()
                     ->dehydrated(),
 
                 TextInput::make('state')
                     ->readOnly()
+                    ->required()
                     ->dehydrated(),
 
                 TextInput::make('is_active')
